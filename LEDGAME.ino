5bc7b2f1f7cee8c88fc08b97c23b075594222ac1
dzhekzzzz <96267843+dzhekzzzz@users.noreply.github.com>
2022-08-23 18:28:37 +0300

    Update LEDGAME.ino

--- LEDGAME.ino
+++ LEDGAME.ino
@@ -2,15 +2,14 @@
 #include <stdio.h> 
 int a = 1;
 int score = 0;
+int noButtPress = 0;
 int num;
 int buttInit[4] = {4, 7, 10, 12} ;
 int ledInit[4] = {3, 9, 5, 2} ;
 int butt[5];
 
-
-
-void setup() {
-  
+void setup() 
+{  
     Serial.begin(9600); 
     pinMode(ledInit[1], OUTPUT);
     pinMode(ledInit[2], OUTPUT);
@@ -19,8 +18,7 @@ void setup() {
     pinMode(buttInit[1], INPUT);
     pinMode(buttInit[2], INPUT);
     pinMode(buttInit[3], INPUT);
-    pinMode(buttInit[0], INPUT);
-    
+    pinMode(buttInit[0], INPUT);   
 }
 
 class Timer
@@ -60,85 +58,57 @@ class Timer
   {
      buttWait();
   }
-  };
+};
 
-
-  
 Timer timer; 
 Timer ledTimer;
-//
-//bool isButtonPressed(int num);
-//{
-//  return (button[num] == 1);
-//}
-//
-//int checkButton(int num){
-//  if (digitalRead(buttInit[1]) == HIGH) {
-//  butt[num] = true 
-//  }
-//}
-//
-//checkBUtton(num)
-//while (!isButtonPressed(num)){
-//  
-//  checkBUtoon(num);
-//  if timerisRun break abo return;
-//  }
 
-//буфер с нажатиями кнопок
-void buttPress() {
-     unsigned long  newTimeLed;
-     timer.i_oldLedTimer = millis();
+bool isTimerRun()
+{
+  unsigned long  newTimeLed;
+  newTimeLed = millis();
+  if ( (newTimeLed - timer.i_oldLedTimer) >= ledTimer.limitValue )
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+  
+}
+
+//ожидаем нажатие кнопки 
+void buttPress()
+{
+  noButtPress = 0;
+  timer.i_oldLedTimer = millis();
    
   while (butt[num] == 0)
   {
-     newTimeLed = millis();
-  if (digitalRead(buttInit[1]) == HIGH) {
-    butt[1] = 1;
-    butt[2] = 0;
-    butt[3] = 0;
+
+  if (digitalRead(buttInit[num]) == HIGH) {
+    butt[num] = num;
     break;
     } 
-  if (digitalRead(buttInit[2]) == HIGH) { 
-    butt[1] = 0;
-    butt[2] = 2;
-    butt[3] = 0;
-    break;
-      }  
-  if (digitalRead(buttInit[3]) == HIGH) {
-    butt[1] = 0;
-    butt[2] = 0;
-    butt[3] = 3;
-    break;
-        }
-   else if ((newTimeLed - timer.i_oldLedTimer) >= ledTimer.limitValue)
+    
+   else if ( isTimerRun() == false )
     {
-     butt[4] = 1;
+       noButtPress = 1;
      break;
     }
   }
   
   }
 
-//включение рандомной кнопки
-int random_led() {
+//включение рандомной дампочки
+int random_led()
+{
    randomSeed(analogRead(0));
    num = rand() % 3 + 1; 
-   
-   if (num == 1) {
-    digitalWrite(ledInit[1], HIGH);
-    }
-   else if (num == 2) {
-    digitalWrite(ledInit[2], HIGH);
-    } 
-   else if (num == 3) {
-     digitalWrite(ledInit[3], HIGH);
-    }
-   else {
-    digitalWrite(ledInit[0], HIGH);
-    }
-    return num;
-  }
+   digitalWrite(ledInit[num], HIGH);   
+   return num;
+}
 
 
 //выключаю все кнопки 
@@ -150,29 +120,22 @@ void led_off()
    digitalWrite(ledInit[0], LOW);
   }  
 
-//ждем нажатие кнопки
+//выдаем результат в зависимости от нажатой кнопки 
 void buttWait()
 {
       buttPress();
-      if (butt[num] == num)
+      if (num == butt[num])
       {
         Serial.print("                                                                  CORRECT! \n");
         score += 1;
       }    
-        else if (butt[4] == 1) 
+        else if (noButtPress == 1)
         {
           Serial.print("                                                          NO BUTTON PRESS DETECTED \n");
         }
-          else if ( (butt[num] != num) )
-           {
-            Serial.print("                                                                INCORRECT((( \n");
-           }
-     
 }
 
-
-
-    //очистка буфера
+//очистка буфера
     void clearbutt()
     {
         butt[0] = 1; 
@@ -182,6 +145,7 @@ void buttWait()
       }
     }
 
+//ждем кнопку старт
 void buttsWait()
 {
   while (butt[0] == 0)
@@ -243,19 +207,3 @@ void loop() {
     break;
   }
 }
-
-for (int p = 0, p < 5, p++;)
-{
-  butt[i] = buttindex;
-}
-
-//
-//int index = randomLed();
-//int buttons[4];
-//int leds[3];
-//buttons[0];
-//index = 0;
-//buttons[index];
-//buttons[index] "" leds[index];
-//butons[index] = 0;
-//index++;
